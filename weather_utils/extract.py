525306f9c3e8986ec3e01746b288b1c5d7c74b6b
ihabsafouat <kiethenbundy2@gmail.com>
2025-05-07 13:48:15 +0100

    V1 extract

---
+++ weather_utils/extract.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_weather_data():
+    API_KEY = 'd3c4ef767d79e23fb341bb85b804759f'
+    CITY = 'Casablanca'
+    URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+    response = requests.get(URL)
+    data = response.json()
+    
+    with open('/tmp/weather_raw.json', 'w') as f:
+        f.write(response.text)
