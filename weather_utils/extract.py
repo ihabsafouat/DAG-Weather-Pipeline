c9fafdd906ee04609df4cb108e6bf9341e313e99
Safouat Ihab <kiethenbundy2@gmail.com>
2025-05-19 11:11:11 +0100

    Update extract.py

--- weather_utils/extract.py
+++ weather_utils/extract.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_weather_data():
-    API_KEY = 'd3c4ef767d79e23fb341bb85b804759f'
+    API_KEY = 'd3c************************' #for security
     CITY = 'Casablanca'
     URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
